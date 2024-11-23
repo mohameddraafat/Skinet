5f880f39ea7e7d82250f3ca1aafad67e98cc4208
Mohamed Raafat <mohamed.raafat@softec.ai>
2024-11-23 12:39:09 +0200

    Add project files.

---
+++ Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,15 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class StoreContext(DbContextOptions options) : DbContext(options)
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
